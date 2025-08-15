1263d9344da80dac9369bc2aeb546f3e1376c002
krigjo25 <krigjo25@gmail.com>
2025-08-15 11:30:53 +0200

    Refactor timeline rendering and visibility toggle logic for improved clarity

--- frontend/src/components/academic/Academic.vue
+++ frontend/src/components/academic/Academic.vue
@@ -1,21 +1,18 @@
 <template>
     <section v-if="timeline.isLoaded" 
         class="timeline-container component-blue timeline-container">
-        <section :class="['flex-wrap-row-align-items-end-justify-space-evenly', 'component-blue']">
-                <Card v-for="data in timeline.even" :key="data.id"
-                    :data="data"
-                    v-show="data.content.isVisible"/>
-        </section>
+
         <section class="flex-wrap-row-justify-space-evenly component-blue timeline-line">
             <Timeline
-            :btn ="btn" :cls="[]"
             :data="timeline.timelineRange"
-            @toggle-visibility="toggleVisibility(data.id)"/>
+            :cls="[['flex-column-align-items-center', 'timeline-item'],
+                'title-h2', ['timeline-input-label', 'timeline-input']]"
+            @toggle-visibility="toggleVisibility"/>
         </section>
         <section class="flex-wrap-row-justify-space-evenly component-blue">
-                <Card v-for="data in timeline.odd" :key="data.id"
+                <Card v-for="data in timeline.timelines" :key="data.id"
                     :data="data"
-                    v-show="data.content.isVisible"/>
+                    v-show="!!data.content.isVisible"/>
         </section>
     </section>  
 </template>
@@ -26,26 +23,16 @@
     import { timelineStore } from '@/stores/timelineStore.js';
 
     const timeline = timelineStore();
-    const btn = 
-    {
-        cls: 'timeline-btn',
-        action: (id) => toggleVisibility(id)
-    };
+
 
     const toggleVisibility = (id) => {
         const data = timeline.timelines;
 
         data.forEach(item => {
-            if (item.id !== id) item.content.isVisible = false;
-
+            if (item.id != id) item.content.isVisible = false;
             else item.content.isVisible = !item.content.isVisible;
-            console.warn("Toggling visibility for ID:", id, item);
         });
-        const item = data.find(item => item.id === id);
-        
-        
-
-        console.log("Timeline after toggling:", data);
+        console.warn("Toggling visibility for ID:", data[id]);
     };
     console.warn("Timeline data on load:", timeline.timelines);
 </script>
