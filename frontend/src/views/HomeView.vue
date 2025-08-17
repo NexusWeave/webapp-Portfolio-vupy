b9e2e0af293357782c39529e09b30b18a47ba967
krigjo25 <krigjo25@gmail.com>
2025-08-17 15:48:13 +0200

    Fix import path for Academic component and comment out Portfolio import in HomeView

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,11 +6,10 @@
         <Academiic/>
     </section>
     <section>
-        <Portfolio />
     </section>
 </template>
 <script setup>
-    import Portfolio from '@/components/Portfolio.vue';
+    //import Portfolio from '@/components/Portfolio.vue';
     import Achievements from '@/components/Achievements.vue';
-    import Academiic from '@/components/academic/Academic.vue';
+    import Academiic from '@/components/Academic.vue';
 </script>
