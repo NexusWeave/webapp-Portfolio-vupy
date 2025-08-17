b0b6c3fb3e1acf9cf5c936b8672a8651c8836055
krigjo25 <krigjo25@gmail.com>
2025-08-17 13:25:00 +0200

    Add Achivements component, API data, and store for timeline management

---
+++ frontend/src/components/Achivements.vue
@@ -0,0 +1,48 @@
+<template>
+    <section v-if="timeline.isLoaded" 
+        class="timeline-container component-blue timeline-container">
+
+        <section class="flex-wrap-row-justify-space-evenly component-blue timeline-line">
+            <Timeline
+            :data="timeline.timelineRange"
+            :cls="[['flex-column-align-items-center', 'timeline-item'],
+                'title-h2', ['timeline-input-label', 'timeline-input']]"
+            @toggle-visibility="toggleVisibility"/>
+        </section>
+        <h3 class="timeline-h3">
+            <Year v-for="data in timeline.timelines" :key="data.id"
+                    :year="data.year" :isVisible="data.isVisible"/>
+        </h3>
+        <section class="flex-wrap-row-justify-space-evenly component-blue">
+                <Card v-for="data in timeline.timelines" :key="data.id"
+                    :data="data"/>
+        </section>
+    </section>  
+</template>
+<script setup>
+    import { timelineStore } from '@/stores/academicStore.js';
+
+    import Year from '@/components/Date/Year.vue';
+    import Card from '@/components/academic/Card.vue';
+    import Timeline from '@/components/academic/Timeline.vue';
+
+    const timeline = AchivementTimelineStore();
+
+
+    const toggleVisibility = (id) => {
+        const data = timeline.timelines;
+        id = parseInt(id);
+
+        data.forEach(item => {
+            if (item.id === id) 
+            
+            {
+                item.isVisible = true;
+                console.log(item.id, id, item);
+            }
+            else item.isVisible = false;
+        });
+        console.warn(`Toggling visibility for ID:${id}`, data[id].isVisible);
+    };
+    //console.warn("Timeline data on load:", timeline.timelines);
+</script>
---
+++ frontend/src/services/achivements-api.js
@@ -0,0 +1,41 @@
+const timeline =
+[
+    {
+        id: 0,
+        year: 2008,
+        content:
+        [
+            {
+                title: "Embarking on a self-guided journey to learn web development",
+                description: `during this phase i started to learn about html's best
+                practices. How to use semantic HTML tags effectively and how 
+                to apply CSS styles to the elements on the page.`
+            },
+        ],
+    },
+    {
+        id: 1,
+        year: 2020,
+        content:
+        [
+            {
+                title: "Dykke inn i en verden av backend",
+                description: `I denne fasen begynte jeg å lære om backend-utvikling,
+                Startet med python, gjennom boken "How To Learn Python Within One Day - Migel Firtsman,
+                Videre fortsatte jeg med integrering av Discord bots.`
+            },
+        ],
+    },
+    {
+        id: 2,
+        year: 2024,
+        content:
+        [
+            {
+                title: "Starten på forfølgelsen av drømmen",
+                description: `Denne fasen markerer begynnelsen på
+                den profesjonelle reisen min som utvikler.`
+            },
+        ],
+    },
+]
