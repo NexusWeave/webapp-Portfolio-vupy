a4354216f48458654b832aa3c4a87438ecd701f3
krigjo25 <krigjo25@gmail.com>
2025-08-21 19:25:36 +0200

    Refactor BusinessCard.vue to replace div with section for semantic structure and improve class handling

--- frontend/src/components/portfolio/BusinessCard.vue
+++ frontend/src/components/portfolio/BusinessCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :class="cls[0]">
+    <section :class="cls[0]">
         <div v-for="lang in data.lang" :key="lang.id" 
             :class="cls[1]">
             <Figure
@@ -7,19 +7,22 @@
                 :cls="['tech-figure', 'tech-img']"
             />
         </div>
-        <h3 :class="[cls[3]]" v-if="Array.isArray(data.name)">{{ data.name[1] }}</h3>
-        <h3 :class="[cls[3]]" v-else>{{ data.name }}</h3>
-        <span :class="[cls[4]]">
-            <time v-bind:datetime="data.date">{{ data.date }}</time>
-        </span>
+        <section class="flex-wrap-row">
+            <h3 :class="[cls[2]]" v-if="Array.isArray(data.name)">{{ data.name[1] }}</h3>
+            <h3 :class="[cls[2]]" v-else>{{ data.name }}</h3>
+            <span :class="[cls[3]]">
+                <time v-bind:datetime="data.date">{{ data.date }}</time>
+            </span>
+        </section>
 
         <section class="flex-column">
-            <p :class="[cls[3]]">{{ data.description }}</p>
+            <p :class="[cls[4]]">{{ data.description }}</p>
 
-            <Navigation :data="data.anchor"
-            :cls="[cls[6]]" />
+            <Navigation :cls="cls[5]"
+            :data="data.anchor"
+             />
         </section>
-    </div>
+    </section>
 </template>
 <script setup>
     import { defineProps } from 'vue';
@@ -34,10 +37,14 @@
         },
         cls: {
             type: Array,
-            default: () => [['card-container', 'grid-container','flex-wrap-column'], 
-            ['grid-item-1', 'flex-wrap-row-justify-space-between'],
-            'figure-img55', 'grid-item-2', 'grid-item-3', 
-            ['nav-container', 'flex-wrap-row-justify-space-evenly', 'grid-item-4']]
+            default: () => [
+                ['card-container', 'flex-column','flex-wrap-column'], 
+                ['grid-item-1', 'flex-wrap-row-justify-space-between'],
+                'title-h3', 'date-container',  'ts',
+                [ ['nav-bar'], ['nav-list', 'flex-row-align-items-center'],
+                ['nav-item'], ['anchor-item'],
+                'grid-item-4']
+        ]
         }
     });
 
