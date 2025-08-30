d923ab3b59bc985bcbbaeaa04f5351d58929edc6
krigjo25 <krigjo25@gmail.com>
2025-08-30 06:39:01 +0200

    Refactor BusinessCard.vue: format date display and improve code structure

--- frontend/src/components/portfolio/BusinessCard.vue
+++ frontend/src/components/portfolio/BusinessCard.vue
@@ -9,7 +9,9 @@
             <h3 :class="[cls[2]]" v-if="Array.isArray(data.name)">{{ data.name[1] }}</h3>
             <h3 :class="[cls[2]]" v-else>{{ data.name }}</h3>
             <span :class="[cls[3]]">
-                <time v-bind:datetime="data.date">{{ data.date }}</time>
+                <time v-bind:datetime="data.date">
+                    <b>{{ data.date }}</b>
+                </time>
             </span>
         </section>
 
@@ -21,13 +23,17 @@
 
             <Navigation :cls="cls[5]"
             :data="data.anchor" toggle="anchor" />
+
+            <Tags v-for="lang in data.lang" :key="lang.id" :data="lang" />
         </section>
     </section>
 </template>
 <script setup>
     import { defineProps } from 'vue';
 
+    
     import Figure from '../media/Figure.vue';
+    import Tags from '@/components/utils/Tags.vue';
     import Navigation from '@/components/navigation/NavMenu.vue';
 
     const props = defineProps({
