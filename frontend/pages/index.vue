47a5e512456e147be781f5288dd1f4d110aa3260
krigjo25 <krigjo25@gmail.com>
2025-09-18 19:46:32 +0200

    x

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,20 +4,31 @@
             <h2> Aktuelle innlegg </h2>        
         </section>
         <section>
-            <h2> Aktuelle innlegg </h2>
+            <h2> Trenings innlegg </h2>
         </section>
     </section>
     <section :class="['flex-wrap-row-justify-space-evenly']">
-        
+        <Academic :data="academic"/>
+        <Achievements :data="achievements"/>
+
     </section>
+    <section>
+        <h2> Portefølje </h2>
     <Portfolio 
         :data="portfolio.portfolio"
         toogle="commercal"
     />
+    </section>
 </template>
 <script setup lang="ts">
-    import { portfolioStore } from '~/stores/portfolioStore'
-        const portfolio = portfolioStore();
+
+    import { academicStore } from '~/stores/academicStore';
+    import { portfolioStore } from '~/stores/portfolioStore';
+    import { achievementStore } from '~/stores/achievementsStore';
+
+    const academic = academicStore();
+    const portfolio = portfolioStore();
+    const achievements = achievementStore();
 
     //console.log(portfolio.isLoaded);
 </script>
