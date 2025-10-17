9b4a6fff2d82bc41f452c2a3d7d89cb3717c936d
krigjo25 <krigjo25@gmail.com>
2025-10-17 14:23:54 +0200

    feat(index): Add wrapper and class for article section styling

--- frontend/pages/dev.vue
+++ frontend/pages/dev.vue
@@ -1,11 +1,17 @@
 <template>
-    <section class="flex-column-justify-center-align-center">
-        <article v-for="data in dev" :key="data.id" class="article-dev">
-            <h3>{{ data.title }}</h3>
-            <ContentRenderer :value="data"/>
-        </article>
+    <section class="flex-wrap-row-justify-space-evenly">
+        <section class="flex-column-justify-space-evenly">
+            <article v-for="data in dev" :key="data.id" class="bio">
+                <h3>{{ data.title }}</h3>
+                <ContentRenderer :value="data"/>
+            </article>
 
+        </section>
 
+        <section>
+            <h2>Blogg posts</h2>
+            <p>This page is currently under development. Please check back later for updates!</p>
+        </section>
     </section>
 </template>
 <script setup lang="ts">
