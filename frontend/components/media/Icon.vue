9a19a4fd5aed25276438c5a6f0026aa39b4fb8cf
krigjo25 <krigjo25@gmail.com>
2025-10-20 10:12:00 +0200

    refactor(Utils/icon): Migrate to TypeScript and remove redundancy     -   Refactored the Icon utility module to improve type safety and     code cleanliness.
    * **Type Safety**
        -   Converted the utility function/module from **JavaScript to
        TypeScript**.
    
    * **Cleanup**
        -   Removed **redundant or unused code blocks**, streamlining the
        utility's logic and improving readability.

--- frontend/components/media/Icon.vue
+++ frontend/components/media/Icon.vue
@@ -1,20 +1,15 @@
 <template>
-    <span :class="cls[0]">
-        <span :class="cls[1]" :aria-label="label"></span> 
-        </span>
+    <span :class="cls" :aria-label="label"></span> 
 </template>
-<script setup>
-    import { defineProps } from 'vue';
+<script lang="ts" setup>
 
-    const props = defineProps({
-        label: {
-            type: [String],
-            required: false
-        },
-        cls: {
-            type: [Array],
-            required: false,
-        }
+    // --- Importing dependencies ---
+    import type { iconProps } from '@/types/props';
 
+    const props = withDefaults(defineProps<iconProps>(),
+    {
+        cls: () => [],
+        label: () => 'An icon'
     });
+
 </script>
