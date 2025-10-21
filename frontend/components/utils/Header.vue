2b8b20525e19e67ba2807bdc1c88121db62b3fed
krigjo25 <krigjo25@gmail.com>
2025-10-21 11:57:31 +0200

    refactor(media/Figure): Migrate to TypeScript     -   Refactored the core logic within the Header component to improve     type safety and performance by migrating from JavaScript to TypeScript.
    * **Language Migration**
        -   Converted the component's script logic from **JavaScript to
        TypeScript** for robust type checking.
    
    * **Social Media Navigation**
        -   Introducing a new component for social media navigation. This
        enhances the user experience by providing a dedicated Call to Action
        (CTA) for social links and improving accessibility.

--- frontend/components/utils/Header.vue
+++ frontend/components/utils/Header.vue
@@ -1,20 +1,29 @@
 <template>
-    <section class="flex-wrap-row-align-items-center">
+    <section class="flex-wrap-row-justify-space-around">
         <section>
             <NavigationNavMenu :data="data.logo" :cls="['logo-bar', 'logo-list', 'logo-item']" toggle="anchor" />
         </section>
         <NavigationNavMenu :data="data.menu" toggle="router"/>
-    </section>
-    <section class="slogan-wrapper">
-            <h1>
-                <span v-for="text in data.title" :key="text">
-                    {{ text }}
-                </span>
-            </h1>
+
+        <section class="flex-column-justify-center-align-center">
+            <h2>Social Media Links / Contact</h2>
+        <NavigationNavMenu :data="SocialMedia"
+            toggle="anchor"
+            :cls="[['some-bar', 'flex-wrap-row'],['some-ul', 'flex-wrap-row'],'some-li']"
+        />
         </section>
+    </section>
+    
+    <section class="slogan-wrapper">
+        <h1>
+            <span v-for="text in data.title" :key="text">{{ text }} </span>
+        </h1>
+    </section>
+
+    
 </template>
 
-<script setup>
+<script lang="ts" setup>
 
     const data =
     {
@@ -58,4 +67,32 @@
             },
         ]
     }
+
+    const SocialMedia =
+    [
+        {
+            type: ['linkedin','external'],
+            href: 'https://www.linkedin.com/in/krigjo25',
+        },
+        {
+            type: ['github','external'],
+            href: 'https://www.github.com/krigjo25',
+        },
+        {
+            type: ['mail','external'],
+            href: 'mailto:krigjo25@outlook.com',
+        },
+        {
+            type: ['ytube','external'],
+            href: 'https://www.youtube.com/@krigjo25',
+        },
+        {
+            type: ['facebook','external'],
+            href: 'https://www.facebook.com/krigjo25',
+        },
+        {
+            type: ['instagram','external'],
+            href: 'https://www.instagram.com/krigjo25',
+        }
+    ];
 </script>
