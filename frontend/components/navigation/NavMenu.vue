5df813f4c51e6cb52f86e8f2d2620158f118b90d
krigjo25 <krigjo25@gmail.com>
2025-10-23 16:01:58 +0200

    refactor(NavMenu): Remove redundant pagination logic     -   Removed unnecessary and redundant pagination-related code and     logic from the NavMenu component. This logic was incorrectly placed     in the navigation menu and belongs in components responsible for     displaying paginated lists, improving the component's focus and     simplicity.

--- frontend/components/navigation/NavMenu.vue
+++ frontend/components/navigation/NavMenu.vue
@@ -3,6 +3,7 @@
         <ul :class="cls[1]">
             <li v-for="item in data" :key="item.id"
                     :class="cls[2]">
+
             <template v-if="isRouterLink">
                 <NuxtLink :to="item.href" :class="[item.cls, 'nav-link']">
                     {{ item.label }}
@@ -12,12 +13,6 @@
             <template v-else-if="isAnchor">
                     <NavigationAnchor :data="item" :cls="[item.cls]"/>
             </template>
-
-            <template v-else-if="isPagination">
-                <NavigationButton :data="btn[0]"/>
-                <span :class="cls[3]"> {{ activePage > 0 ?  activePage + ' / ' + totalPages : '' }}</span>
-                <NavigationButton :data="btn[1]"/>
-            </template>
             </li>
         </ul>
     </nav>
@@ -26,7 +21,7 @@
 <script lang="ts" setup>
 
     //  --- Importing dependencies & type definitions ---
-    import { ref, watch, computed } from 'vue';
+    import { computed } from 'vue';
     import type { NavProp } from '@/types/props';
 
     //  --- Props Definition ---
@@ -37,49 +32,12 @@
         cls: () => [['nav-bar', 'flex-wrap-row'], [['nav-list', 'flex-wrap-row-justify-center'], 'flex-row-align-items-center'], 'nav-item', 'anchor-item'],
     });
 
-    const data = props.data;
+    const data = computed(() => props.data);
 
     const isAnchor = computed(() => props.toggle === 'anchor');
     const isRouterLink = computed(() => props.toggle === 'router');
-    const isPagination = computed(() => props.toggle === 'pagination');
 
-    //  --- Pagination Logic ---
-
-    const activePage = ref(props.activePage);
-    const totalPages = computed(() => props.totalPage);
-    const btn = computed(() =>
-        [
-            {
-                id: 0,
-                label: 'Forrige',
-                cls: ['button', 'pagnition-btn'],
-                disabled: activePage.value <= 1 ? 'disabled' : false,
-                action: () => { if (activePage.value > 1)  activePage.value--; },
-
-            },
-            {
-                id: 1,
-                label: 'Neste',
-                cls: ['button', 'archive-btn'],
-                disabled: activePage.value >= totalPages.value? 'disabled' : false,
-                action: () => { if (activePage.value < totalPages.value)  activePage.value++; },
-            },
-        ]);
-
-    //  Watch for changes in the 'data' prop
-    watch(() => props.activePage, (newValue) => 
-    {
-        activePage.value = newValue;
-    });
-
-    //  --- Emits definition ---
-    const emit = defineEmits(['update']);
-
-    watch(() => activePage, (newValue) => 
-    {
-        emit('update', newValue);
-    }, { immediate: true });
 
     //  --- Debug logic
-    console.log("NavigationMenu loaded with data: ", data, isAnchor, isRouterLink);
+    //console.log("NavigationMenu loaded with data: ", data, isAnchor, isRouterLink);
 </script>
