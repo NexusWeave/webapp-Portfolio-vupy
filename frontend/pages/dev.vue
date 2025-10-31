acee2224980d115c88a30f28432822616b84657f
krigjo25 <krigjo25@gmail.com>
2025-10-31 06:29:53 +0100

    feat(dev): Introduce developer sidebar and optimize key usage     -   Implemented a new, dedicated sidebar for developer information     and improved rendering performance by updating key usage in loops.
    * **Developer Sidebar**
        -   Added a new sidebar component including:
            -   Developer image.
            -   Employer references.
            -   List of technical skills.
    
    * **Sidebar Logic**
        -   Implemented the logic controlling the display and functionality
        of the new sidebar.
    
    * **Performance Improvement**
        -   Updated the `:key` usage in component loops (e.g., article loops)
        to use array indexing instead of **unique IDs**, which significantly
        improves DOM update efficiency.

--- frontend/pages/dev.vue
+++ frontend/pages/dev.vue
@@ -1,21 +1,71 @@
 <template>
     <section class="flex-wrap-row-justify-space-evenly">
+        <section class="dev-profile">
+            <MediaFigure
+                :data="{
+                    type: 'jpg',
+                    src: 'media/images/carousel/20240903_165612.jpg',
+                    alt: 'Portrett av Kristoffer Gjøsund',
+                }"
+                :cls="['dev-avatar']"
+            />
+            <section>
+                <h2> Attest sitater </h2>
+            </section>
+            <section>
+                <h2> Skill Bar</h2>
+
+                <UtilsProgress 
+                    :value="totalProgress"
+                    label="Fullstack Utvikler"
+                />
+                <UtilsProgress v-for="(data, i) in sortedProgressList" :key="i" 
+                    :value="data.value"
+                    :label="data.name"
+                />
+            </section>
+            
+        </section>
         <section class="flex-column-justify-space-evenly">
-            <article v-for="data in dev" :key="data.id" class="bio">
-                <h3>{{ data.title }}</h3>
-                <ContentRenderer :value="data"/>
+            <article v-for="(data, i) in dev" :key="i" class="bio">
+                <h3 v-if="i === 1">{{ data.title }}</h3>
+                <ContentRenderer v-if="i === 1" :value="data" />
             </article>
 
+            <article v-for="(data, i) in dev" :key="i" class="bio">
+                <h3 v-if="i !== 1">{{ data.title }}</h3>
+                <ContentRenderer v-if="i !== 1" :value="data"/>
+            </article>
         </section>
 
     </section>
+    
 </template>
+
 <script setup lang="ts">
 
+    const progressList =
+    [
+        { name:'C', value: 25 },
+        { name:'GO', value: 5 },    
+        { name:'Python', value: 40 },
+        { name:'C# / .NET', tech:"c", value: 25 },
+        { name:'SQL / Databaser', value: 50 },
+        { name:'Javascript / TypeScript', tech:"javascript", value: 26 },
+        
+        
+    ];
+
+    const n = progressList.length;
+    const totalProgress = computed(() => (progressList.reduce((acc, item) => acc + item.value, 0) + n) / n);
+    const sortedProgressList = computed(() => progressList.slice().sort((a, b) => b.value - a.value));
+    //  --- Dev Data Logic
     const path = 'dev';
     const {data: dev} = await useAsyncData('dev', () => 
     {
         return queryCollection(path).all();
     });
+
+    //  --- Debugging Logic ---
     //console.log(dev.value);
 </script>
