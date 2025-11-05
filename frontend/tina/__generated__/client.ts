4c480f6d1cc86fbef50e474012fa00215056da13
krigjo25 <krigjo25@gmail.com>
2025-11-05 18:25:33 +0100

    feat(__generated__): Auto generated files from TinaCMS

---
+++ frontend/tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: '/run/media/krigjo25/Documents/Telia Sky/projects/Repository/Webapps/webapp-Portfolio-vupy/frontend/tina/__generated__/.cache/1762363305465', url: 'https://content.tinajs.io/1.6/content/db61a396-6aa4-4652-a2fe-945aa8c080b8/github/CMS', token: '9e88ca4413e8a633b696b61afb3f3fd8b7c4b560', queries,  });
+export default client;
+  
