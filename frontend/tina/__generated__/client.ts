748ee16c32a1bb65f8a46e476880b582bdbdea08
krigjo25 <krigjo25@gmail.com>
2025-11-05 19:50:53 +0100

    x

--- frontend/tina/__generated__/client.ts
+++ frontend/tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/run/media/krigjo25/Documents/Telia Sky/projects/Repository/Webapps/webapp-Portfolio-vupy/frontend/tina/__generated__/.cache/1762364048672', url: 'https://content.tinajs.io/1.6/content/db61a396-6aa4-4652-a2fe-945aa8c080b8/github/CMS', token: '81bc438b846fd537e653e3c412b3d2b5a8a3efa2', queries,  });
+export const client = createClient({ cacheDir: '/run/media/krigjo25/Documents/Telia Sky/projects/Repository/Webapps/webapp-Portfolio-vupy/frontend/tina/__generated__/.cache/1762368371985', url: 'https://content.tinajs.io/1.6/content/db61a396-6aa4-4652-a2fe-945aa8c080b8/github/CMS', token: '81bc438b846fd537e653e3c412b3d2b5a8a3efa2', queries,  });
 export default client;
   
